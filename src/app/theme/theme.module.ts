fb1e953ff2d21989c9a2f8b0d815958d2e9075c9
jarvisharishkumar <63893689+jarvisharishkumar@users.noreply.github.com>
2023-02-20 15:51:57 +0530

    5 files modified

--- src/app/theme/theme.module.ts
+++ src/app/theme/theme.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NgbCollapseModule],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
 })
